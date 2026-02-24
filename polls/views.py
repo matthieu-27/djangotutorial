e6b3ee0b5c9fd619f1aa89d88dec325e3d5edafe
matthieu-27 <matthieu27@pm.me>
2026-02-24 15:52:05 +0100

    feat: add /all endpoint to list polls

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpRequest, HttpResponse
+from .models import Question
+from django.template import loader
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def all_polls(request: HttpRequest) -> HttpResponse:
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/all.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
