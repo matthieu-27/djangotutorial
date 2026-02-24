8b500344e1e55cae978c8515958c23d64a533dbd
matthieu-27 <matthieu27@pm.me>
2026-02-24 12:58:30 +0100

    feat: add admin filters

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ["pub_date", "question_text"]
+    list_display = ["question_text", "pub_date"]
+    ordering = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_filter = ["question", "choice_text", "votes"]
+    list_display = ["choice_text"]
+    ordering = ["votes"]
+    search_fields = ["choice_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
