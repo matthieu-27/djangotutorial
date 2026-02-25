9eaa38c3a798297b8e061b97aae290cfaa242c60
m.couderc <m.couderc@komet-is.fr>
2026-02-25 13:20:09 +0100

    feat: add link to statistics page

--- polls/urls.py
+++ polls/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
     path("<int:question_id>/frequency/", views.frequency, name="frequency"),
+    path("<int:question_id>/statistics/", views.statistics, name="statistics"),
 ]
